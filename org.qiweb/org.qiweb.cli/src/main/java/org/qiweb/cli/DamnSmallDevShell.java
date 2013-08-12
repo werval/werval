410ef62a406852e890af7a774d370f383d92258e
Paul Merlin <paul@nosphere.org>
2013-08-12 09:57:54 +0200

    CLI: first working prototype

---
+++ org.qiweb/org.qiweb.cli/src/main/java/org/qiweb/cli/DamnSmallDevShell.java
@@ -0,0 +1,436 @@
+/*
+ * Copyright (c) 2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.qiweb.cli;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.PosixParser;
+import org.apache.commons.exec.DefaultExecutor;
+import org.apache.commons.exec.PumpStreamHandler;
+import org.codeartisans.java.toolbox.Strings;
+import org.qiweb.api.exceptions.QiWebException;
+import org.qiweb.devshell.DevShell;
+import org.qiweb.devshell.JNotifyWatcher;
+import org.qiweb.devshell.QiWebDevShellException;
+import org.qiweb.runtime.util.ClassLoaders;
+import org.qiweb.spi.dev.DevShellSPIAdapter;
+import org.qiweb.spi.dev.Watcher;
+
+/**
+ * Damn Small QiWeb DevShell.
+ */
+// TODO QUID Use embedded ant tasks under the hood to ensure cross-platform compatibility.
+public final class DamnSmallDevShell
+{
+
+    private static class SPI
+        extends DevShellSPIAdapter
+    {
+
+        private final URL[] classpath;
+        private final Set<File> sources;
+        private final File classesDir;
+
+        private SPI( URL[] classPath, Set<File> sources, Watcher watcher, File classesDir )
+        {
+            super( classPath, sources, watcher );
+            this.classpath = classPath;
+            this.sources = sources;
+            this.classesDir = classesDir;
+        }
+
+        @Override
+        protected void doRebuild()
+        {
+            try
+            {
+                DamnSmallDevShell.rebuild( classpath, sources, classesDir );
+            }
+            catch( Exception ex )
+            {
+                throw new RuntimeException( ex.getMessage(), ex );
+            }
+        }
+    }
+
+    private static class ShutdownHook
+        implements Runnable
+    {
+
+        private final DevShell devShell;
+        private final File tmpDir;
+
+        private ShutdownHook( DevShell devShell, File tmpDir )
+        {
+            this.devShell = devShell;
+            this.tmpDir = tmpDir;
+        }
+
+        @Override
+        public void run()
+        {
+            devShell.stop();
+            org.codeartisans.java.toolbox.io.Files.deleteSilently( tmpDir );
+        }
+    }
+    // figlet -f rectangles  "QiWeb DevShell"
+    private static final String LOGO = ""
+                                       + " _____ _ _ _ _     _      ____          _____ _       _ _ \n"
+                                       + "|     |_| | | |___| |_   |    \\ ___ _ _|   __| |_ ___| | |\n"
+                                       + "|  |  | | | | | -_| . |  |  |  | -_| | |__   |   | -_| | |\n"
+                                       + "|__  _|_|_____|___|___|  |____/|___|\\_/|_____|_|_|___|_|_|\n"
+                                       + "   |__|                                QiWeb v" + BuildVersion.VERSION + "-" + BuildVersion.COMMIT + "\n";
+
+    public static void main( String[] args )
+    {
+        System.out.println( LOGO );
+
+        Options options = declareOptions();
+
+        CommandLineParser parser = new PosixParser();
+        try
+        {
+            CommandLine cmd = parser.parse( options, args );
+
+            // Handle --help
+            if( cmd.hasOption( "help" ) )
+            {
+                PrintWriter out = new PrintWriter( System.out );
+                printHelp( options, out );
+                out.flush();
+                System.exit( 0 );
+            }
+
+            // Handle --version
+            if( cmd.hasOption( "version" ) )
+            {
+                System.out.println(
+                    "Damn Small QiWeb DevShell v" + BuildVersion.VERSION + "\n"
+                    + "Git commit: " + BuildVersion.COMMIT + ( BuildVersion.DIRTY ? " (DIRTY)" : "" ) + ", built on: " + BuildVersion.DATE + "\n"
+                    + "Java version: " + System.getProperty( "java.version" ) + ", vendor: " + System.getProperty( "java.vendor" ) + "\n"
+                    + "Java home: " + System.getProperty( "java.home" ) + "\n"
+                    + "Default locale: " + Locale.getDefault().toLanguageTag() + ", platform encoding: " + System.getProperty( "file.encoding" ) + "\n"
+                    + "OS name: " + System.getProperty( "os.name" ) + ", version: " + System.getProperty( "os.version" ) + ", arch: " + System.getProperty( "os.arch" ) );
+                System.exit( 0 );
+            }
+
+            // Handle commands
+            List<String> commands = cmd.getArgList();
+            if( commands.isEmpty() )
+            {
+                commands = Collections.singletonList( "run" );
+            }
+            System.out.println( "COMMANDS: " + commands );
+            Iterator<String> it = commands.iterator();
+            while( it.hasNext() )
+            {
+                String command = it.next();
+                switch( command )
+                {
+                    case "new":
+                        break;
+                    case "clean":
+                        break;
+                    case "run":
+                    default:
+                        break;
+                }
+            }
+
+            // Temporary directory
+            final File tmpDir = new File( cmd.getOptionValue( 't', "build/devshell.tmp" ) );
+            final File classesDir = new File( tmpDir, "classes" );
+            Files.createDirectories( classesDir.toPath() );
+
+            // Sources
+            String[] sourcesPaths = cmd.hasOption( 's' ) ? cmd.getOptionValues( 's' ) : new String[]
+            {
+                "src/main/java",
+                "src/main/resources"
+            };
+            Set<File> sources = new LinkedHashSet<>();
+            for( String sourcePath : sourcesPaths )
+            {
+                sources.add( new File( sourcePath ) );
+            }
+
+            // Classpath
+            List<URL> classpathList = new ArrayList<>();
+            // First, current classpath
+            classpathList.addAll( ClassLoaders.urlsOf( DamnSmallDevShell.class.getClassLoader() ) );
+            // Then either command line provided or defaults
+            if( cmd.hasOption( 'c' ) )
+            {
+                for( String url : cmd.getOptionValues( 'c' ) )
+                {
+                    classpathList.add( new URL( url ) );
+                }
+            }
+            else
+            {
+                classpathList.add( new File( "lib" ).toURI().toURL() );
+            }
+            // Append Application sources
+            for( File source : sources )
+            {
+                classpathList.add( source.toURI().toURL() );
+            }
+            // Append compilation output
+            classpathList.add( classesDir.toURI().toURL() );
+            URL[] classpath = classpathList.toArray( new URL[ classpathList.size() ] );
+
+            // Apply System Properties
+            Properties systemProperties = cmd.getOptionProperties( "D" );
+            for( Map.Entry entry : systemProperties.entrySet() )
+            {
+                System.setProperty( entry.getKey().toString(), entry.getValue().toString() );
+            }
+
+            System.out.println( "Loading..." );
+
+            // Deploy JNotify
+            JNotifyWatcher.deployNativeLibraries( tmpDir );
+            Watcher watcher = new JNotifyWatcher();
+
+            // First build
+            rebuild( classpath, sources, classesDir );
+
+            // Run DevShell
+            final DevShell devShell = new DevShell( new SPI( classpath, sources, watcher, classesDir ) );
+            Runtime.getRuntime().addShutdownHook( new Thread( new ShutdownHook( devShell, tmpDir ), "qiweb-devshell-shutdown" ) );
+            devShell.start();
+        }
+        catch( IllegalArgumentException | ParseException | IOException ex )
+        {
+            PrintWriter out = new PrintWriter( System.err );
+            printHelp( options, out );
+            out.flush();
+            System.exit( -1 );
+        }
+        catch( QiWebException ex )
+        {
+            ex.printStackTrace( System.err );
+            System.err.flush();
+            System.exit( -1 );
+        }
+    }
+    private static final DefaultExecutor executor = new DefaultExecutor();
+
+    /* package */ static void rebuild( URL[] classpath, Set<File> sources, File classesDir )
+    {
+        System.out.println( "Compiling Application..." );
+        ByteArrayOutputStream javacOutput = new ByteArrayOutputStream();
+        try
+        {
+            // Collect java files
+            ByteArrayOutputStream findJavaOutput = new ByteArrayOutputStream();
+            for( File source : sources )
+            {
+                if( source.exists() )
+                {
+                    org.apache.commons.exec.CommandLine findJava = new org.apache.commons.exec.CommandLine( "find" );
+                    findJava.addArgument( source.getAbsolutePath() );
+                    findJava.addArgument( "-type" );
+                    findJava.addArgument( "f" );
+                    findJava.addArgument( "-iname" );
+                    findJava.addArgument( "*.java" );
+
+                    executor.setStreamHandler( new PumpStreamHandler( findJavaOutput ) );
+                    executor.execute( findJava );
+                }
+            }
+            // Write list in a temporary file
+            String javaFiles = findJavaOutput.toString( "UTF-8" );
+            if( !Strings.isEmpty( javaFiles ) )
+            {
+                File javaListFile = new File( classesDir, ".devshell-java-list" );
+                try( FileWriter writer = new FileWriter( javaListFile ) )
+                {
+                    writer.write( javaFiles );
+                    writer.close();
+                }
+                // Compile
+                org.apache.commons.exec.CommandLine javac = new org.apache.commons.exec.CommandLine( "javac" );
+                javac.addArgument( "-encoding" );
+                javac.addArgument( "UTF-8" );
+                javac.addArgument( "-source" );
+                javac.addArgument( "1.7" );
+                javac.addArgument( "-d" );
+                javac.addArgument( classesDir.getAbsolutePath() );
+                javac.addArgument( "-classpath" );
+                String[] classpathStrings = new String[ classpath.length ];
+                for( int idx = 0; idx < classpath.length; idx++ )
+                {
+                    classpathStrings[idx] = classpath[idx].toURI().toASCIIString();
+                }
+                javac.addArgument( Strings.join( classpathStrings, ":" ) );
+                javac.addArgument( "@" + javaListFile.getAbsolutePath() );
+
+                executor.setStreamHandler( new PumpStreamHandler( javacOutput ) );
+                executor.execute( javac );
+            }
+        }
+        catch( IOException | URISyntaxException ex )
+        {
+            String output = javacOutput.toString(); // utf-8
+            throw new QiWebDevShellException( "Unable to rebuild" + ( Strings.isEmpty( output ) ? "" : "\n" + output ), ex );
+        }
+    }
+
+    @SuppressWarnings( "static-access" )
+    private static Options declareOptions()
+    {
+        Option classpathOption = OptionBuilder
+            .withArgName( "element" )
+            .hasArgs()
+            .withDescription( "Set application classpath element. "
+                              + "Use this option several times to declare a full classpath. "
+                              + "Defaults to 'lib' in current directory." )
+            .withLongOpt( "classpath" )
+            .create( 'c' );
+
+        Option sourcesOption = OptionBuilder
+            .withArgName( "directory" )
+            .hasArgs()
+            .withDescription( "Set application sources directories. "
+                              + "Use this option several times to declare multiple sources directories. "
+                              + "Defaults to 'src/main/java' and 'src/main/resources' in current directory." )
+            .withLongOpt( "sources" )
+            .create( 's' );
+
+        Option tmpdirOption = OptionBuilder
+            .withArgName( "directory" )
+            .hasArgs()
+            .withDescription( "Set temporary directory. Defaults to 'build/devshell.tmp' in current directory." )
+            .withLongOpt( "tmpdir" )
+            .create( 't' );
+
+        Option propertiesOption = OptionBuilder.withArgName( "property=value" )
+            .hasArgs( 2 )
+            .withValueSeparator()
+            .withDescription( "Define a system property. "
+                              + "Use this option several times to define multiple system properties. "
+                              + "Particularly convenient when used to override application configuration." )
+            .withLongOpt( "define" )
+            .create( 'D' );
+
+        Option debugOption = OptionBuilder
+            .withDescription( "Enable debug output." )
+            .withLongOpt( "debug" )
+            .create( 'd' );
+
+        Option versionOption = OptionBuilder
+            .withDescription( "Display version information." )
+            .withLongOpt( "version" )
+            .create();
+
+        Option helpOption = OptionBuilder
+            .withDescription( "Display help information." )
+            .withLongOpt( "help" )
+            .create();
+
+        Options options = new Options();
+        options.addOption( classpathOption );
+        options.addOption( sourcesOption );
+        options.addOption( tmpdirOption );
+        options.addOption( propertiesOption );
+        options.addOption( debugOption );
+        options.addOption( versionOption );
+        options.addOption( helpOption );
+        return options;
+    }
+
+    private static class OptionsComparator
+        implements Comparator<Option>
+    {
+
+        private static final List<String> optionsOrder = Arrays.asList( new String[]
+        {
+            "classpath",
+            "sources",
+            "tmpdir",
+            "define",
+            "debug",
+            "version",
+            "help",
+        } );
+
+        @Override
+        public int compare( Option o1, Option o2 )
+        {
+            Integer o1idx = optionsOrder.indexOf( o1.getLongOpt() );
+            Integer o2idx = optionsOrder.indexOf( o2.getLongOpt() );
+            return o1idx.compareTo( o2idx );
+        }
+    }
+
+    private static void printHelp( Options options, PrintWriter out )
+    {
+        HelpFormatter help = new HelpFormatter();
+        help.setOptionComparator( new OptionsComparator() );
+        help.printUsage( out, 80, "devshell [options] [command(s)]" );
+        out.print( "\n"
+                   + "  The Damn Small QiWeb DevShell\n"
+                   + "  - do not manage dependencies ;\n"
+                   + "  - do not allow you to extend the build ;\n"
+                   + "  - do not assemble applications.\n" );
+        help.printWrapped( out, 80, 2,
+                           "\n"
+                           + "Meaning you have to manage your application dependencies and assembly yourself. "
+                           + "Theses limitations make this DevShell suitable for quick prototyping only. "
+                           + "Prefer the Gradle or Maven build systems integration." );
+        out.println( "\n  org.qiweb.cli is part of the QiWeb Development Kit - http://qiweb.org" );
+        out.println( "\n"
+                     + "Commands:\n\n"
+                     + "  new [appdir]  Create a new skeleton application in the 'appdir' directory.\n"
+                     + "  clean         Delete devshell temporary directory, see 'tmpdir' option.\n"
+                     + "  run           Run the QiWeb Development Shell.\n"
+                     + "\n"
+                     + "  If no command is specified, 'run' is assumed." );
+        out.println( "\n"
+                     + "Options:"
+                     + "\n" );
+        help.printOptions( out, 80, options, 2, 2 );
+        help.printWrapped( out, 80, 2,
+                           "\n"
+                           + "All paths are relative to the current working directory, "
+                           + "except if they are absolute of course." );
+    }
+}
