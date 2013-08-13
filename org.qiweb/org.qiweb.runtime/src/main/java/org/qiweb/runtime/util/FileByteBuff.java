06279a1c26cb4d613a092cf30de148dff0ae426b
Paul Merlin <paul@nosphere.org>
2013-08-13 12:55:03 +0200

    License: applied source headers

--- org.qiweb/org.qiweb.runtime/src/main/java/org/qiweb/runtime/util/FileByteBuff.java
+++ org.qiweb/org.qiweb.runtime/src/main/java/org/qiweb/runtime/util/FileByteBuff.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (c) 2013 the original author or authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.qiweb.runtime.util;
 
 import io.netty.buffer.AbstractByteBuf;
@@ -358,7 +373,8 @@ public final class FileByteBuff
     }
 
     @Override
-    public ByteBuffer internalNioBuffer(int index, int length) {
+    public ByteBuffer internalNioBuffer( int index, int length )
+    {
         throw new UnsupportedOperationException( NOT_SUPPORTED );
     }
 }
