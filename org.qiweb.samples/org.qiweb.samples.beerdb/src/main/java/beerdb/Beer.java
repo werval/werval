4d45f759efc48e980d02d73f0b92c4aa71676a05
Paul Merlin <paul@nosphere.org>
2013-09-22 16:21:18 +0200

    BeerDB Sample: fixed database table names to plural

--- org.qiweb.samples/org.qiweb.samples.beerdb/src/main/java/beerdb/Beer.java
+++ org.qiweb.samples/org.qiweb.samples.beerdb/src/main/java/beerdb/Beer.java
@@ -21,8 +21,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 @Entity
+@Table( name = "beers" )
 public class Beer
 {
 
