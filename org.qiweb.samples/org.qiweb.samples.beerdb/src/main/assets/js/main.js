34dea52f2a9a3e0f1ba25e2475b56785cb75104b
Paul Merlin <paul@nosphere.org>
2013-09-24 11:45:11 +0200

    BeerDB Sample: Boilerplate code to get UI dev workflow OK

--- org.qiweb.samples/org.qiweb.samples.beerdb/src/main/assets/js/main.js
+++ org.qiweb.samples/org.qiweb.samples.beerdb/src/main/assets/js/main.js
@@ -1,4 +1,12 @@
 
+var list_breweries = function() {
+    $.getJSON("/api/breweries", function(data) {
+        $("#output").prepend(JSON.stringify(data));
+    });
+};
+
 $(document).ready(function() {
     console.log("Beer Database UI");
+    $("#main").append("<h1>Welcome to the Beer Database QiWeb Sample!</h1><pre id=\"output\"></pre>");
+    list_breweries();
 });
