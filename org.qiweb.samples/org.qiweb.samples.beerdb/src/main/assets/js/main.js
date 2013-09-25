680b7e86a96abaeab93ffc9dba0160ae22ddfe4c
Paul Merlin <paul@nosphere.org>
2013-09-25 14:09:08 +0200

    BeerDB Sample: UI prototypes

--- org.qiweb.samples/org.qiweb.samples.beerdb/src/main/assets/js/main.js
+++ org.qiweb.samples/org.qiweb.samples.beerdb/src/main/assets/js/main.js
@@ -1,12 +1,10 @@
 
-var list_breweries = function() {
-    $.getJSON("/api/breweries", function(data) {
-        $("#output").prepend(JSON.stringify(data));
-    });
-};
+jQuery(function($) {
 
-$(document).ready(function() {
-    console.log("Beer Database UI");
-    $("#main").append("<h1>Welcome to the Beer Database QiWeb Sample!</h1><pre id=\"output\"></pre>");
-    list_breweries();
+    'use strict';
+
+    $(document).ready(function() {
+        console.log("Beer Database UI");
+    });
+
 });
