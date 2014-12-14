8bd7576459f0e74574e9da244e9427d1481d625c
Paul Merlin <paul@nosphere.org>
2014-12-15 00:34:30 +0100

    Build, quiet by default, verbose on error

--- build.sh
+++ build.sh
@@ -22,22 +22,22 @@ fi
 
 # core
 $FIGLET io.werval
-./gradlew -b io.werval/build.gradle install check idea -x licenseMain -x checkstyleMain -x test
+./gradlew -q --stacktrace -b io.werval/build.gradle install check idea -x licenseMain -x checkstyleMain -x test
 echo "Built."
 
 # gradle plugin
 $FIGLET io.werval.gradle
-./gradlew -b io.werval.gradle/build.gradle install check idea -x checkstyleMain -x test
+./gradlew -q --stacktrace -b io.werval.gradle/build.gradle install check idea -x checkstyleMain -x test
 echo "Built."
 
 # maven plugin
 $FIGLET io.werval.maven
-$MAVEN -f io.werval.maven/pom.xml install dependency:sources -DskipTests -Dgpg.skip
+$MAVEN -q -e -f io.werval.maven/pom.xml install dependency:sources -DskipTests -Dgpg.skip
 echo "Built."
 
 # modules
 $FIGLET io.werval.modules
-./gradlew -b io.werval.modules/build.gradle install check idea -x licenseMain -x checkstyleMain -x test
+./gradlew -q --stacktrace -b io.werval.modules/build.gradle install check idea -x licenseMain -x checkstyleMain -x test
 echo "Built."
 
 echo ""
